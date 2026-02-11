f779b71c041657dd12e046ce8b799f44698e3503
Hoochymoochy <Khaydenroberts22@gmail.com>
2026-02-11 16:05:31 -0700

    added ui to switch for the game

---
+++ game_type.js
@@ -0,0 +1,10 @@
+const gametype = document.querySelector("#chess960");
+const gametype2 = document.querySelector("#regular");
+
+gametype.addEventListener("click", () => {
+    window.location.href = "index2.html";
+});
+
+gametype2.addEventListener("click", () => {
+    window.location.href = "index3.html";
+});
