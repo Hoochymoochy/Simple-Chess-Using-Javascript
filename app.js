4d7db60a4d3cfb5f4b13a6ceeec6cb5dc74829d9
Hoochymoochy <Khaydenroberts22@gmail.com>
2026-02-11 15:05:51 -0700

    added ches960

--- app.js
+++ app.js
@@ -8,16 +8,56 @@ const width = 8
 let playerTurn = 'black';
 playerDetails.textContent = 'black'
 
-const startPieces = [
-    Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook,
-    Pawn, Pawn, Pawn, Pawn, Pawn, Pawn, Pawn, Pawn,
-    '', '', '', '', '', '', '', '',
-    '', '', '', '', '', '', '', '',
-    '', '', '', '', '', '', '', '',
-    '', '', '', '', '', '', '', '',
-    Pawn, Pawn, Pawn, Pawn, Pawn, Pawn, Pawn, Pawn,
-    Rook, Knight, Bishop, King, Queen, Bishop, Knight, Rook,
-]
+function chess960() {
+    const startPieces = [ 
+        null, null, null, null, null, null, null, null,
+        Pawn, Pawn, Pawn, Pawn, Pawn, Pawn, Pawn, Pawn,
+        '', '', '', '', '', '', '', '', 
+        '', '', '', '', '', '', '', '', 
+        '', '', '', '', '', '', '', '', 
+        '', '', '', '', '', '', '', '', 
+        Pawn, Pawn, Pawn, Pawn, Pawn, Pawn, Pawn, Pawn,
+        null, null, null, null, null, null, null, null
+    ];
+
+    const randomChoice = (arr) =>
+        arr[Math.floor(Math.random() * arr.length)];
+
+    const emptyIndices = () =>
+        startPieces
+            .map((piece, index) =>
+                index < 8 && piece === null ? index : null
+            )
+            .filter(i => i !== null);
+
+    const darkSquares = [0, 2, 4, 6];
+    const lightSquares = [1, 3, 5, 7];
+
+    startPieces[randomChoice(darkSquares)] = Bishop;
+    startPieces[randomChoice(lightSquares)] = Bishop;
+
+    startPieces[randomChoice(emptyIndices())] = Queen;
+
+    startPieces[randomChoice(emptyIndices())] = Knight;
+    startPieces[randomChoice(emptyIndices())] = Knight;
+
+    const remaining = emptyIndices().sort((a, b) => a - b);
+
+    startPieces[remaining[0]] = Rook;
+    startPieces[remaining[1]] = King;
+    startPieces[remaining[2]] = Rook;
+
+    for (let i = 0; i < 8; i++) {
+        startPieces[56 + i] = startPieces[i];
+    }
+
+    return startPieces;
+}
+
+const startPieces = chess960();
+
+
+
 
 function createBoard() {
     startPieces.forEach((startPiece, i) => {
